58b47852023e78f736e8450e59bb2e8b447b4aa1
dkssyd13 <vladkim2002@gmail.com>
2025-04-01 15:59:37 +0900

    ElementController 와 요소 추가, 다운로드 테스트 코드 작성

---
+++ src/test/java/com/capstone2025/team4/backend/controller/ElementControllerTest.java
@@ -0,0 +1,173 @@
+package com.capstone2025.team4.backend.controller;
+
+import com.capstone2025.team4.backend.domain.design.Element;
+import com.capstone2025.team4.backend.domain.design.SlideElement;
+import com.capstone2025.team4.backend.domain.design.Type;
+import com.capstone2025.team4.backend.infra.aws.S3Entity;
+import com.capstone2025.team4.backend.infra.aws.S3Service;
+import com.capstone2025.team4.backend.infra.security.CustomUserDetailService;
+import com.capstone2025.team4.backend.infra.security.config.SecurityConfig;
+import com.capstone2025.team4.backend.infra.security.jwt.JwtService;
+import com.capstone2025.team4.backend.mock.WithCustomMockUser;
+import com.capstone2025.team4.backend.repository.ElementRepository;
+import com.capstone2025.team4.backend.service.design.ElementService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.lang.reflect.Field;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ElementController.class)
+@Import(SecurityConfig.class)
+@WithCustomMockUser
+class ElementControllerTest {
+
+    @MockitoBean
+    DesignController designController;
+
+    @MockitoBean
+    ElementService elementService;
+    
+    @MockitoBean
+    S3Service s3Service;
+
+    @MockitoBean
+    ElementRepository elementRepository;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockitoBean
+    JwtService jwtService;
+
+    @MockitoBean
+    CustomUserDetailService customUserDetailService;
+
+    @Test
+    void addNewElementSuccess() throws Exception {
+        //given
+        MockMultipartFile multipart = new MockMultipartFile("file", "test.png", "image/png", new FileInputStream(new File("/Users/vladkim/Pictures/test.png")));
+        given(s3Service.upload(multipart)).willReturn("tempUrl");
+
+        SlideElement slideElement = createSlideElement();
+        given(elementService.addUserElementToSlide(1L, 1L, "tempUrl", Type.IMAGE, 0L, 0L, 3.14, 1920L, 1080L)).willReturn(slideElement);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                multipart("/element/add") // 여기를 post()가 아니라 multipart()로 변경
+                        .file(multipart)
+                        .param("userId", "1")
+                        .param("slideId", "1")
+                        .param("elementId", "1")
+                        .param("type", "IMAGE")
+                        .param("x", "0")
+                        .param("y", "0")
+                        .param("angle", "3.14")
+                        .param("width", "1920")
+                        .param("height", "1080")
+        );
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.id").value(1L));
+
+    }
+
+    @Test
+    void addNewElementBadArg() throws Exception {
+        //given
+        MockMultipartFile multipart = new MockMultipartFile("file", "test.png", "image/png", new FileInputStream(new File("/Users/vladkim/Pictures/test.png")));
+        given(s3Service.upload(multipart)).willReturn("tempUrl");
+
+        SlideElement slideElement = createSlideElement();
+        given(elementService.addUserElementToSlide(1L, 1L, "tempUrl", Type.IMAGE, 0L, 0L, 3.14, 1920L, 1080L)).willReturn(slideElement);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                multipart("/element/add") // 여기를 post()가 아니라 multipart()로 변경
+                        .file(multipart)
+                        .param("userId", "")
+                        .param("slideId", "1")
+                        .param("elementId", "1")
+                        .param("type", "IMAGE")
+                        .param("x", "0")
+                        .param("y", "0")
+                        .param("angle", "3.14")
+                        .param("width", "1920")
+                        .param("height", "1080")
+        );
+        //then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.data.userId").value("유저 정보는 필수입니다"));
+    }
+
+    @Test
+    void getFileSuccess() throws Exception {
+        //given
+        given(elementRepository.findById(1L)).willReturn(Optional.of(createElement()));
+        given(s3Service.findByUrl("tempUrl")).willReturn(S3Entity.builder().url("tempUrl").s3Key("s3Key").originalFileName("fileName").build());
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                get("/element/1")
+        );
+
+        //then
+        resultActions
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void getFileBadArg() throws Exception {
+        //given
+        given(elementRepository.findById(1L)).willReturn(Optional.of(createElement()));
+        given(s3Service.findByUrl("tempUrl")).willReturn(S3Entity.builder().url("tempUrl").s3Key("s3Key").originalFileName("fileName").build());
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                get("/element/")
+        );
+
+        //then
+        resultActions
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    private SlideElement createSlideElement() throws Exception {
+        SlideElement slideElement = SlideElement.builder()
+                .element(Element.builder().url("tempUrl").build())
+                .build();
+        Field id = SlideElement.class.getDeclaredField("id");
+        id.setAccessible(true);
+        id.set(slideElement, 1L);
+        return slideElement;
+    }
+
+    private Element createElement() {
+        return Element.builder()
+                .url("tempUrl")
+                .build();
+    }
+}
