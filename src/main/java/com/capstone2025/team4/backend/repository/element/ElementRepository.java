b0058907cfda95c6ace8de4100f9055475bf09cf
dkssyd13 <vladkim2002@gmail.com>
2025-04-13 21:44:38 +0900

    element repository 추가

--- src/main/java/com/capstone2025/team4/backend/repository/element/ElementRepository.java
+++ src/main/java/com/capstone2025/team4/backend/repository/element/ElementRepository.java
@@ -1,6 +1,6 @@
-package com.capstone2025.team4.backend.repository;
+package com.capstone2025.team4.backend.repository.element;
 
-import com.capstone2025.team4.backend.domain.design.element.Element;
+import com.capstone2025.team4.backend.domain.element.Element;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ElementRepository extends JpaRepository<Element, Long> {
