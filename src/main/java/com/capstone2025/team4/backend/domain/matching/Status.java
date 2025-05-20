5ebb8f6ae6a28114080f6c0007439605b7419105
dkssyd13 <vladkim2002@gmail.com>
2025-05-20 17:46:21 +0900

    (수정) 매칭 관련 코드 삭제

--- src/main/java/com/capstone2025/team4/backend/domain/matching/Status.java
+++
@@ -1,8 +0,0 @@
-package com.capstone2025.team4.backend.domain.matching;
-
-public enum Status {
-    WAITING,
-    ACCEPTED,
-    FINISHED,
-    REJECTED
-}
--- src/main/java/com/capstone2025/team4/backend/domain/post/Category.java
+++
@@ -1,7 +0,0 @@
-package com.capstone2025.team4.backend.domain.post;
-
-public enum Category {
-    SELF_INTRO, // 자기소개서
-    PORTFOLIO, // 포트폴리오
-    SERVICE // 실제 자기 서비스 홍보
-}
